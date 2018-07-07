22dc2f8fecc06ca810106db9297f7ec68e8bd2ee
Andrew <atsmith3@illinois.edu>
2018-07-07 18:55:45 -0500

    Blink wokring + GDB Tested

--- STM32F103/main.c
+++ STM32F103/main.c
@@ -1,40 +1,26 @@
 /*
-********************************************************************************
-*                                                                              *
-* Copyright (c) 2017 Andrea Loi                                                *
-*                                                                              *
-* Permission is hereby granted, free of charge, to any person obtaining a      *
-* copy of this software and associated documentation files (the "Software"),   *
-* to deal in the Software without restriction, including without limitation    *
-* the rights to use, copy, modify, merge, publish, distribute, sublicense,     *
-* and/or sell copies of the Software, and to permit persons to whom the        *
-* Software is furnished to do so, subject to the following conditions:         *
-*                                                                              *
-* The above copyright notice and this permission notice shall be included      *
-* in all copies or substantial portions of the Software.                       *
-*                                                                              *
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   *
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     *
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL      *
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER   *
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      *
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER          *
-* DEALINGS IN THE SOFTWARE.                                                    *
-*                                                                              *
-********************************************************************************
-* Your code goes here.                                                         *
-********************************************************************************
-*/
+ * STM32F103
+ *
+ * Blink LED
+ *
+ */
 
 
-
-#include "common.h"
 #include "regs.h"
 
-
-
 int main(void){
-  for(;;) {}
+    // Emable GPIO C
+    (*(volatile uint32_t *)(0x40021018)) |= (1 << 4);
+
+    // Set GPIO C13 as output
+    (*(volatile uint32_t *)(0x40011004)) |= (0x02 << ((13 - 8) * 4));
+ 
+    while(1) {
+        for(int i = 0; i < 50000; i++) __asm__("nop");
+        (*(volatile uint32_t *)(0x4001100C)) |= (0x01 << 13);
+        for(int i = 0; i < 50000; i++) __asm__("nop");
+        (*(volatile uint32_t *)(0x4001100C)) &= ~(0x01 << 13);
+    }
 }
 
 
---
+++ STM32F103/timer.h
@@ -0,0 +1,11 @@
+/*
+ * Illini Formula Electric
+ *    STM32F103
+ *    Timers API
+ *
+ *    07/06/2018
+ *    Andrew Smith
+ *    atsmith3@illinois.edu
+ */
+
+
