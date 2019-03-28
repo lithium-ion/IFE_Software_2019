76086171f228df47c02f01ad1aff4352a8ff8af2
JoeLopez333 <joseal2@illinois.edu>
2019-03-27 19:44:00 -0500

    Some code that makes

--- SAFETY/SafetyBoard/Src/stm32f1xx_it.c
+++ SAFETY/SafetyBoard/Src/stm32f1xx_it.c
@@ -71,7 +71,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
-extern CAN_HandleTypeDef hcan;
+
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -224,20 +224,6 @@ void EXTI3_IRQHandler(void)
   /* USER CODE BEGIN EXTI3_IRQn 1 */
 
   /* USER CODE END EXTI3_IRQn 1 */
-}
-
-/**
-  * @brief This function handles CAN RX1 interrupt.
-  */
-void CAN1_RX1_IRQHandler(void)
-{
-  /* USER CODE BEGIN CAN1_RX1_IRQn 0 */
-
-  /* USER CODE END CAN1_RX1_IRQn 0 */
-  HAL_CAN_IRQHandler(&hcan);
-  /* USER CODE BEGIN CAN1_RX1_IRQn 1 */
-
-  /* USER CODE END CAN1_RX1_IRQn 1 */
 }
 
 /**
