90ee952dde9876762ade8e1e51244f70c012d249
njtucza2 <njtucza2@illinois.edu>
2019-03-28 18:52:56 -0500

    safety CAN ID's

--- CAN/CAN_ID's/can_id.h
+++ CAN/CAN_ID's/can_id.h
@@ -1,4 +1,5 @@
 #include can_id_bms.h       //0x007 to 0x00A
 #include can_id_charger.h   //
 #include can_id_dash.h		//0x00F to 0x012
-#include can_id_rienhart.h
+#include can_id_rienhart.h // 0x0A0 to 0x0CF
+#include can_id_safety.h  // 0x0D0 to  
---
+++ CAN/CAN_ID's/can_id_safety.h
@@ -0,0 +1,53 @@
+/*
+*
+* Nicholas Tuczak
+* CAN ID's for the Safety Board
+*
+*
+*
+*/
+
+
+
+
+#define FAULTS 0x0D0
+/*
+*
+* SIZE = 3 Bytes 
+*  Byte # 	|	Type 	| Description 			| Name
+------------------------------------------------------
+* 	0 		| 	Boolean	| 	0xFF = Fault present| BMS
+*			|			| 	0x00 = no FAULT 	|
+-----------------------------------------------------
+*	1		| 	Boolean	|	0xFF = Fault present| IMD 
+*			| 			|	0x00 = no fault 	|
+------------------------------------------------------
+* 	2 		| 	Boolean	|	0xFF = Fault Presnet| BSPD
+*			|			| 	0x00 = no FAULT 	| 
+*
+*
+*/
+
+#define PRECHARGE 0x0D1
+/*
+*
+* Size = 1 Byte 
+*	Byte 	| Type 		| Description 			| Name
+------------------------------------------------------
+*	0		|	Boolean	|	0xFF = Charged		| Precharge
+			|			|	0x00 = not charged 	|	
+*
+*
+*/
+
+#define ENABLE	0x0D2
+/*
+*
+* Size = 1 Byte 
+*	Byte 	| Type 		| Description 			| Name
+------------------------------------------------------
+*	0		|	Boolean	|	0xFF = Enabled 		| Enabled
+			|			|	0x00 = not Enabled 	|	
+*
+*
+*/
