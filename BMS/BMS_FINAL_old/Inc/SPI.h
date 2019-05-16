7e8d37cc7b0118f8418c053b80b612f0b40acd62
Matt Vlasaty <mmvlasaty@gmail.com>
2019-05-15 23:00:53 -0500

    previous version of BMS master code

---
+++ BMS/BMS_FINAL_old/Inc/SPI.h
@@ -0,0 +1,8 @@
+#include "stm32f1xx.h"
+#include "stdbool.h"
+#include "stdlib.h"
+#include "string.h"
+
+void SPI_Init(void);
+bool SPIWrite(uint8_t *writeBuffer, uint8_t totalBytes);
+bool SPIWriteRead(uint8_t *writeBuffer, uint8_t *readBuffer, uint8_t totalBytes);
